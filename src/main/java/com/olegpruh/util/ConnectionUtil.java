386c49224650bcf6813926585cf0c541ea229869
Oleg Pruh <olegpruh1@gmail.com>
2022-11-06 18:01:46 +0000

    The README.md has been modified

--- src/main/java/com/olegpruh/util/ConnectionUtil.java
+++ src/main/java/com/olegpruh/util/ConnectionUtil.java
@@ -6,11 +6,10 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 public class ConnectionUtil {
-    private static final String URL = "jdbc:mysql://localhost:3306/taxi?useUnicode=true&"
-            + "useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
-    private static final String USERNAME = "root";
-    private static final String PASSWORD = "root";
-    private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String URL = "DATABASE URL";
+    private static final String USERNAME = "USERNAME";
+    private static final String PASSWORD = "PASSWORD";
+    private static final String JDBC_DRIVER = "JDBC DRIVER";
 
     static {
         try {
